efc12523f1498d10b64f199a90baf3de0c0acae6
JeongDong Seo <sjd001@empas.com>
2024-11-27 17:08:47 +0900

    commid14

--- routes/images.js
+++ routes/images.js
@@ -5,12 +5,20 @@ var fs = require('fs');
 var path = require('path');
 var multer = require('multer');
 var upload = multer({dest: path.join(__dirname, '..', 'uploads')});
-var {S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+var {S3Client, PutObjectCommand, ListObjectsV2Command} = require("@aws-sdk/client-s3");
 const s3Client = new S3Client({region: 'ap-northeast-2'});
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  s3Client.send(
+    new ListObjectsV2Command({
+      Bucket: "test-bucket-20241127-1",
+      MaxKeys: 50,
+    })).then((data) => {
+      res.send(data.Contents);
+    }).catch((error) => {
+      console.log(error);
+    });
 });
 
 router.post('/', upload.single('new-image'), function(req, res, next) {
